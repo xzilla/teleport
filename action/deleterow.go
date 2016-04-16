3bdfba8054a47fb978c9309a469119ddff80b0c3
Pedro Franceschi <pedrohfranceschi@gmail.com>
2016-04-15 21:35:52 -0300

    Implement DELETE row.

---
+++ action/deleterow.go
@@ -0,0 +1,35 @@
+package action
+
+import (
+	"encoding/gob"
+	"fmt"
+)
+
+type DeleteRow struct {
+	SchemaName string
+	TableName  string
+	PrimaryKey Row
+}
+
+// Register type for gob
+func init() {
+	gob.Register(&DeleteRow{})
+}
+
+func (a *DeleteRow) Execute(c Context) error {
+	_, err := c.Tx.Exec(
+		fmt.Sprintf(
+			`DELETE FROM "%s"."%s" WHERE "%s" = $1;`,
+			a.SchemaName,
+			a.TableName,
+			a.PrimaryKey.Column.Name,
+		),
+		a.PrimaryKey.Value,
+	)
+
+	return err
+}
+
+func (a *DeleteRow) Filter(targetExpression string) bool {
+	return IsInTargetExpression(&targetExpression, &a.SchemaName, &a.TableName)
+}
