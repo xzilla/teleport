0b487e9f2bcbbfa35c197fab7156f3ad1f68c359
Pedro Franceschi <pedrohfranceschi@gmail.com>
2016-05-02 12:26:30 -0300

    Fix tests.

--- applier/applier_test.go
+++ applier/applier_test.go
@@ -41,8 +41,6 @@ func init() {
 		os.Exit(1)
 	}
 
-	stubAction := &StubAction{}
-
 	stubBatch = &database.Batch{
 		Id:          "",
 		Status:      "waiting_apply",
@@ -53,23 +51,30 @@ func init() {
 		StorageType: "db",
 	}
 
-	stubBatch.SetActions([]action.Action{stubAction})
-
 	targets := make(map[string]*client.Client)
 
 	for key, target := range config.Targets {
 		targets[key] = client.New(target)
 	}
 
-	applier = New(db, 100)
+	applier = New(db, 2)
 }
 
 // StubAction implements Action
-type StubAction struct{}
+type StubAction struct{
+	Success bool
+}
 
 func (a *StubAction) Execute(c *action.Context) error {
-	_, err := c.Tx.Exec("CREATE TABLE test (id INT); INSERT INTO test (id) VALUES (3);")
-	return err
+	if a.Success {
+		_, err := c.Tx.Exec("CREATE TABLE IF NOT EXISTS test (id SERIAL PRIMARY KEY, content TEXT); INSERT INTO test (content) VALUES ('asd');")
+		return err
+	} else {
+		_, err := c.Tx.Exec("asdasdasd;")
+		return err
+	}
+
+	return nil
 }
 
 func (a *StubAction) Filter(targetExpression string) bool {
@@ -80,32 +85,158 @@ func (a *StubAction) NeedsSeparatedBatch() bool {
 	return false
 }
 
-func TestApplyBatch(t *testing.T) {
+func TestApplyBatchDb(t *testing.T) {
 	db.Db.Exec(`
 		DROP TABLE test;
 		TRUNCATE teleport.batch;
 	`)
 
 	tx := db.NewTransaction()
+	stubBatch = database.NewBatch("db")
+	stubBatch.DataStatus = "waiting_apply"
+	stubBatch.SetActions([]action.Action{&StubAction{true}})
 	stubBatch.InsertQuery(tx)
 	tx.Commit()
 
-	_, err := applier.applyBatch(stubBatch)
+	shouldContinue, err := applier.applyBatch(stubBatch)
 
 	if err != nil {
 		t.Errorf("applyBatch returned error: %v", err)
 	}
 
-	var testId string
-	db.Db.Get(&testId, "SELECT id FROM test;")
-
-	if testId != "3" {
-		t.Errorf("test id => %s, want %s", testId, "3")
+	if !shouldContinue {
+		t.Errorf("shouldContinue => false, want true")
 	}
 
-	batches, _ := db.GetBatches("applied", "")
+	var testCount int
+	db.Db.Get(&testCount, "SELECT id FROM test;")
+
+	if testCount != 1 {
+		t.Errorf("test id => %d, want %d", testCount, 1)
+	}
+
+	batches, _ := db.GetBatches("applied", "applied")
 
 	if len(batches) != 1 {
 		t.Errorf("applied batches => %d, want %d", len(batches), 1)
+	}
+}
+
+func TestApplyBatchFs(t *testing.T) {
+	db.Db.Exec(`
+		DROP TABLE test;
+		TRUNCATE teleport.batch;
+	`)
+
+	tx := db.NewTransaction()
+	stubBatch = database.NewBatch("fs")
+	stubBatch.DataStatus = "transmitted"
+	stubBatch.SetActions([]action.Action{&StubAction{true}})
+	stubBatch.InsertQuery(tx)
+	tx.Commit()
+
+	shouldContinue, err := applier.applyBatch(stubBatch)
+
+	if err != nil {
+		t.Errorf("applyBatch returned error: %v", err)
+	}
+
+	if !shouldContinue {
+		t.Errorf("shouldContinue => false, want true")
+	}
+
+	var testCount int
+	db.Db.Get(&testCount, "SELECT count(*) FROM test;")
+
+	if testCount != 1 {
+		t.Errorf("test id => %d, want %d", testCount, 1)
+	}
+
+	batches, _ := db.GetBatches("applied", "applied")
+
+	if len(batches) != 1 {
+		t.Errorf("applied batches => %d, want %d", len(batches), 1)
+	}
+
+	stubBatch.PurgeData()
+}
+
+func TestApplyBatchFsMultipleApplies(t *testing.T) {
+	db.Db.Exec(`
+		DROP TABLE test;
+		TRUNCATE teleport.batch;
+	`)
+
+	tx := db.NewTransaction()
+	stubBatch = database.NewBatch("fs")
+	stubBatch.DataStatus = "transmitted"
+	stubBatch.SetActions([]action.Action{&StubAction{true},&StubAction{true},&StubAction{true},&StubAction{true},&StubAction{true}})
+	stubBatch.InsertQuery(tx)
+	tx.Commit()
+
+	shouldContinue, err := applier.applyBatch(stubBatch)
+
+	if err != nil {
+		t.Errorf("applyBatch returned error: %v", err)
+	}
+
+	if !shouldContinue {
+		t.Errorf("shouldContinue => false, want true")
+	}
+
+	var testCount int
+	db.Db.Get(&testCount, "SELECT count(*) FROM test;")
+
+	if testCount != 5 {
+		t.Errorf("test id => %d, want %d", testCount, 5)
+	}
+
+	batches, _ := db.GetBatches("applied", "applied")
+
+	if len(batches) != 1 {
+		t.Errorf("applied batches => %d, want %d", len(batches), 1)
+	}
+
+	stubBatch.PurgeData()
+}
+
+func TestApplyFailedBatch(t *testing.T) {
+	db.Db.Exec(`
+		DROP TABLE test;
+		TRUNCATE teleport.batch;
+	`)
+
+	tx := db.NewTransaction()
+	stubBatch = database.NewBatch("db")
+	stubBatch.DataStatus = "waiting_apply"
+	stubBatch.SetActions([]action.Action{&StubAction{false}, &StubAction{true}})
+	stubBatch.InsertQuery(tx)
+	tx.Commit()
+
+	shouldContinue, err := applier.applyBatch(stubBatch)
+
+	if err == nil {
+		t.Errorf("applyBatch did not return error")
+	}
+
+	if !shouldContinue {
+		t.Errorf("shouldContinue => false, want true")
+	}
+
+	var testCount int
+	db.Db.Get(&testCount, "SELECT count(*) FROM test;")
+
+	if testCount == 1 {
+		t.Errorf("test id => %d, want 0", testCount, 0)
+	}
+
+	batches, _ := db.GetBatches("waiting_apply", "")
+
+	if len(batches) != 1 {
+		t.Errorf("waiting apply batches => %d, want %d", len(batches), 1)
+	}
+
+	if !batches[0].WaitingReexecution {
+		t.Errorf("waiting apply batch is not waiting reexecution!")
 	}
 }
