2202546869b2b702705a74d55d55240eb937ba88
Thales Mello <thalesmello@gmail.com>
2017-02-24 20:03:58 -0300

    Migrate insert row logic to FallbackUpserter object

--- action/insertrow.go
+++ action/insertrow.go
@@ -47,96 +47,108 @@ func (a *InsertRow) Execute(c *Context) error {
 			return err
 		}
 	} else {
-		// Perform a single insert (upsert)
-		escapedCols := make([]string, 0)
-		escapedRows := make([]string, 0)
-		values := make([]interface{}, 0)
+		return (&FallbackUpserter{}).upsert(a, c)
+	}
 
-		var primaryKeyRow Row
+	return nil
+}
 
-		for i, row := range a.Rows {
-			escapedCols = append(escapedCols, fmt.Sprintf("\"%s\"", row.Column.Name))
-			escapedRows = append(escapedRows, fmt.Sprintf("$%d::%s\"%s\"", i+1, row.Column.GetTypeSchemaStr(a.SchemaName), row.Column.Type))
+type Upserter interface {
+	upsert(*InsertRow, *Context) error
+}
 
-			// Marshall JSON objects as pg driver does not support it
-			if obj, ok := row.Value.(*map[string]interface{}); ok {
-				jsonStr, err := json.Marshal(obj)
+type FallbackUpserter struct{}
 
-				if err != nil {
-					return err
-				}
+func (f *FallbackUpserter) upsert(r *InsertRow, c *Context) error {
+	// Perform a single insert (upsert)
+	escapedCols := make([]string, 0)
+	escapedRows := make([]string, 0)
+	values := make([]interface{}, 0)
 
-				values = append(values, string(jsonStr))
-			} else {
-				values = append(values, row.Value)
+	var primaryKeyRow Row
+
+	for i, row := range r.Rows {
+		escapedCols = append(escapedCols, fmt.Sprintf("\"%s\"", row.Column.Name))
+		escapedRows = append(escapedRows, fmt.Sprintf("$%d::%s\"%s\"", i+1, row.Column.GetTypeSchemaStr(r.SchemaName), row.Column.Type))
+
+		// Marshall JSON objects as pg driver does not support it
+		if obj, ok := row.Value.(*map[string]interface{}); ok {
+			jsonStr, err := json.Marshal(obj)
+
+			if err != nil {
+				return err
 			}
 
-			if row.Column.Name == a.PrimaryKeyName {
-				primaryKeyRow = row
-			}
+			values = append(values, string(jsonStr))
+		} else {
+			values = append(values, row.Value)
 		}
 
-		// Save transaction prior to inserting to rollback
-		// if INSERT fails, so a UPDATE can be tried
-		_, err := c.Tx.Exec(fmt.Sprintf(
-			`SAVEPOINT "%s%s";`,
-			a.SchemaName,
-			a.TableName,
+		if row.Column.Name == r.PrimaryKeyName {
+			primaryKeyRow = row
+		}
+	}
+
+	// Save transaction prior to inserting to rollback
+	// if INSERT fails, so a UPDATE can be tried
+	_, err := c.Tx.Exec(fmt.Sprintf(
+		`SAVEPOINT "%s%s";`,
+		r.SchemaName,
+		r.TableName,
+	))
+
+	if err != nil {
+		return err
+	}
+
+	_, err = c.Tx.Exec(
+		fmt.Sprintf(
+			`
+				INSERT INTO "%s"."%s" (%s) VALUES (%s);
+			`,
+			r.SchemaName,
+			r.TableName,
+			strings.Join(escapedCols, ","),
+			strings.Join(escapedRows, ","),
+		),
+		values...,
+	)
+
+	// Try to UPDATE (upsert) if INSERT fails...
+	if err != nil {
+		// Rollback to SAVEPOINT
+		_, err = c.Tx.Exec(fmt.Sprintf(
+			`ROLLBACK TO SAVEPOINT "%s%s";`,
+			r.SchemaName,
+			r.TableName,
 		))
 
 		if err != nil {
 			return err
 		}
 
-		_, err = c.Tx.Exec(
-			fmt.Sprintf(
-				`
-					INSERT INTO "%s"."%s" (%s) VALUES (%s);
-				`,
-				a.SchemaName,
-				a.TableName,
-				strings.Join(escapedCols, ","),
-				strings.Join(escapedRows, ","),
-			),
-			values...,
-		)
+		updateAction := &UpdateRow{
+			r.SchemaName,
+			r.TableName,
+			primaryKeyRow,
+			r.Rows,
+		}
 
-		// Try to UPDATE (upsert) if INSERT fails...
+		err = updateAction.Execute(c)
+
 		if err != nil {
-			// Rollback to SAVEPOINT
-			_, err = c.Tx.Exec(fmt.Sprintf(
-				`ROLLBACK TO SAVEPOINT "%s%s";`,
-				a.SchemaName,
-				a.TableName,
-			))
+			return err
+		}
+	} else {
+		// Release SAVEPOINT to avoid "out of shared memory"
+		_, err := c.Tx.Exec(fmt.Sprintf(
+			`RELEASE SAVEPOINT "%s%s";`,
+			r.SchemaName,
+			r.TableName,
+		))
 
-			if err != nil {
-				return err
-			}
-
-			updateAction := &UpdateRow{
-				a.SchemaName,
-				a.TableName,
-				primaryKeyRow,
-				a.Rows,
-			}
-
-			err = updateAction.Execute(c)
-
-			if err != nil {
-				return err
-			}
-		} else {
-			// Release SAVEPOINT to avoid "out of shared memory"
-			_, err := c.Tx.Exec(fmt.Sprintf(
-				`RELEASE SAVEPOINT "%s%s";`,
-				a.SchemaName,
-				a.TableName,
-			))
-
-			if err != nil {
-				return err
-			}
+		if err != nil {
+			return err
 		}
 	}
 
